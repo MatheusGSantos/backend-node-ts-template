df67a239fa70d2a35dc6c3c3b4400e50cdd1a6d9
Matheus Gama dos Santos <mths.gm@gmail.com>
2020-11-23 15:57:14 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(routes);
+
+app.listen(3333, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server started on port 3333.');
+});
